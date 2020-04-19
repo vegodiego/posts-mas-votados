0776f73a5e315f41971f758ee5d7c5ab0fe17e22
vegodiego <53877309+vegodiego@users.noreply.github.com>
2020-04-18 23:40:23 -0500

    Updates

---
+++ precache-manifest.a455d5b11a3b899c8524c0ebd5dfe06c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30836fa2c5cf430905d281ea2f4556db",
+    "url": "/posts-mas-votados/index.html"
+  },
+  {
+    "revision": "c28c0bb5c11146e69d86",
+    "url": "/posts-mas-votados/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "758720f1d8d26f9e8573",
+    "url": "/posts-mas-votados/static/css/main.b3bfdb6c.chunk.css"
+  },
+  {
+    "revision": "c28c0bb5c11146e69d86",
+    "url": "/posts-mas-votados/static/js/2.40086e56.chunk.js"
+  },
+  {
+    "revision": "86493497955f2c03ad4534f44ae20baa",
+    "url": "/posts-mas-votados/static/js/2.40086e56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "758720f1d8d26f9e8573",
+    "url": "/posts-mas-votados/static/js/main.44f49744.chunk.js"
+  },
+  {
+    "revision": "a573d62b4daf6629236f",
+    "url": "/posts-mas-votados/static/js/runtime-main.a0d2895b.js"
+  }
+]);
